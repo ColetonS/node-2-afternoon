315062222603ef72b817c7726d9b4464344f2c39
ColetonS <coleton.m.smith@gmail.com>
2019-07-15 15:47:40 -0600

    Hi, Matt.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mc = require('./controllers/messages_controller')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'))
+
+
+app.get('/api/messages', mc.read)
+app.post('/api/messages', mc.create)
+app.put('/api/messages/:id', mc.update)
+app.delete('/api/messages/:id', mc.delete)
+
+const port = 3001
+app.listen(port, () => {
+    console.log(`Existential crisis on port ${port}`)
+})
+
+
